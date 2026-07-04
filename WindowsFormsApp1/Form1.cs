f56091559e35a9bf3f3991fb20ea094cd34cde04
muziiiq <phytonz1807@gmail.com>
2026-07-04 09:42:34 +0800

    Deleted a textbox

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -12,14 +12,6 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            
-        }
+        public Form1() => InitializeComponent();
     }
 }
