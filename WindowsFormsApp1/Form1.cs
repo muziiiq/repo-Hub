58be041bd2f2f455e9514a9d325e838258b91e87
muziiiq <phytonz1807@gmail.com>
2026-07-04 10:15:10 +0800

    Made a message box show hello world

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -13,5 +13,10 @@ namespace WindowsFormsApp1
     public partial class Form1 : Form
     {
         public Form1() => InitializeComponent();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello, World!");
+        }
     }
 }
